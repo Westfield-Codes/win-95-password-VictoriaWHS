af698c3a93361cece10988a17442559badac97bc
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-12-19 16:52:53 +0000

    working

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function setUp(){
+    let buttons = document.getElementById("buttons");
+    let ok = document.createElement("button");
+    ok.innerHTML="Ok";
+    let cancel = document.createElement("button");
+    cancel.innerHTML = "cancel";
+    if (password == 7.3942 && )
 
+    
+}
+
+//If the user "Lawnmower Man" enters "7.3942" as the password
+//and clicks "OK", delete the entire div class box and show http://i.imgur.com/IUVDNyp.jpg.
